bacfc3bbcc5cdc3b040d54e1efd05fce9c518b63
Nuno Góis <github@nunogois.com>
2022-07-29 07:47:52 +0100

    0.0.2 (#3)
    * chore: update proxy-client dependency, bump version
    
    * feat: export context symbol
    
    * add GH workflows

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,6 +8,7 @@ export type {
 } from 'unleash-proxy-client';
 export { UnleashClient, LocalStorageProvider, InMemoryStorageProvider } from 'unleash-proxy-client';
 
+import { ContextStateSymbol } from './context';
 import FlagProvider from './FlagProvider.svelte';
 import useFlag from './useFlag';
 import useFlagsStatus from './useFlagsStatus';
@@ -15,6 +16,14 @@ import useVariant from './useVariant';
 import useUnleashContext from './useUnleashContext';
 import useUnleashClient from './useUnleashClient';
 
-export { FlagProvider, useFlag, useFlagsStatus, useVariant, useUnleashContext, useUnleashClient };
+export {
+	ContextStateSymbol,
+	FlagProvider,
+	useFlag,
+	useFlagsStatus,
+	useVariant,
+	useUnleashContext,
+	useUnleashClient
+};
 
 export default FlagProvider;
